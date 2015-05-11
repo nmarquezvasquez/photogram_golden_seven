0287adf9cbe423e277bbc7636c8904eb7c52a056
nmarquezvasquez <nmarquezvasquez2016@kellogg.northwestern.edu>
2015-05-10 21:46:54 -0500

    Homework Done!

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
 
+  # Routes to CREATE photos
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+  get("/create_photo", { :controller => "photos", :action => "create_row" })
   get("/", { :controller => "photos", :action => "index" })
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
+  # Routes to DESTROY photos
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+
+  # Routes to UPDATE photos
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
 end
